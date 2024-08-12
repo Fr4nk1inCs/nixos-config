f873f1a42b9d12bdeb5d10eefa5ef0beff0da861
Fr4nk1in <fushen@mail.ustc.edu.cn>
2024-08-13 00:00:45 +0800

    [nixvim] init

--- home-manager/cli/neovim/default.nix
+++ home-manager/cli/neovim/default.nix
@@ -3,9 +3,16 @@
   pkgs,
   ...
 }: {
-  programs.neovim = {
+  imports = [
+    ./core
+    ./utils
+    ./extra
+  ];
+
+  programs.nixvim = {
     enable = true;
 
+    enableMan = true;
     defaultEditor = true;
 
     viAlias = true;
@@ -13,126 +20,10 @@
     vimdiffAlias = true;
 
     withNodeJs = true;
-    withPython3 = true;
     withRuby = true;
-
-    extraPackages = with pkgs; [
-      # Dependent binaries
-      gcc
-      unzip
-      gnumake
-      fswatch
-
-      # Make mason.nvim happy :(
-      (python3.withPackages (ps: [ps.pynvim]))
-      cargo
-      go
-
-      # LSPs
-      basedpyright
-      clang-tools
-      gopls
-      jdt-language-server
-      ltex-ls
-      lua-language-server
-      neocmakelsp
-      nil
-      ruff-lsp
-      rust-analyzer-unwrapped
-      shellcheck
-      tailwindcss-language-server
-      taplo
-      texlab
-      tinymist
-      vscode-langservers-extracted
-      yaml-language-server
-
-      nodePackages.vscode-json-languageserver
-      nodePackages.typescript-language-server
-      nodePackages."@astrojs/language-server"
-
-      # Debuggers
-      delve
-      gdb
-      lldb
-      (python3.withPackages (ps: [ps.debugpy]))
-
-      # Formatters
-      alejandra
-      black
-      cmake-format
-      gofumpt
-      gotools
-      isort
-      shfmt
-      stylua
-
-      nodePackages.prettier
-
-      # Tree-sitter
-      tree-sitter
-
-      # Linter
-      cmake-lint
-
-      # Typst
-      typst
-      typst-preview
-      websocat
-    ];
-
-    plugins = [
-      (pkgs.vimPlugins.nvim-treesitter.withPlugins (p:
-        with p; [
-          astro
-          bash
-          c
-          cmake
-          cpp
-          css
-          diff
-          go
-          gomod
-          gosum
-          gowork
-          html
-          java
-          javascript
-          jsdoc
-          json
-          json5
-          jsonc
-          latex
-          llvm
-          lua
-          luadoc
-          luap
-          markdown
-          markdown_inline
-          mlir
-          ninja
-          python
-          query
-          regex
-          ron
-          rst
-          rust
-          tablegen
-          toml
-          tsx
-          typescript
-          verilog
-          vim
-          vimdoc
-          xml
-          yaml
-        ]))
-    ];
   };
 
   programs.zsh.shellAliases = {
     "v" = "nvim";
   };
-
-  xdg.configFile.nvim.source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/home-manager/cli/neovim/nvim";
 }
