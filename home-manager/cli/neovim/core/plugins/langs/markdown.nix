52b05abb563b58365fc271a3844f2b32e91d9ed5
Fr4nk1in <fushen@mail.ustc.edu.cn>
2024-09-02 14:46:29 +0800

    [nixvim] add linting and formatting for markdown

--- home-manager/cli/neovim/core/plugins/langs/markdown.nix
+++ home-manager/cli/neovim/core/plugins/langs/markdown.nix
@@ -3,5 +3,8 @@ _: {
     markview = {
       enable = true;
     };
+
+    none-ls.sources.formatting.prettier.enable = true;
+    none-ls.sources.diagnostics.markdownlint_cli2.enable = true;
   };
 }
