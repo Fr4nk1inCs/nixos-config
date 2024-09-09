48e00a71d46a5c39a8141ca5de582d08b8fbdc42
Fr4nk1in <fushen@mail.ustc.edu.cn>
2024-09-09 14:14:29 +0800

    [nixvim] adopt new lualine configuration format

--- home-manager/cli/neovim/core/plugins/ui/statusline.nix
+++ home-manager/cli/neovim/core/plugins/ui/statusline.nix
@@ -1,31 +1,30 @@
 _: {
   programs.nixvim.plugins.lualine = {
     enable = true;
+    settings = {
+      options = {
+        globalstatus = true;
+        theme = "auto";
+        disabledFiletypes.statusline = ["dashboard"];
+      };
+      extensions = ["neo-tree" "fzf" "toggleterm" "man"];
 
-    globalstatus = true;
-    theme = "auto";
-    disabledFiletypes.statusline = ["dashboard"];
-    extensions = ["neo-tree" "fzf" "toggleterm" "man"];
-
-    sections = {
-      lualine_a = [
-        {
-          name = "mode";
-          icon = "";
-          fmt.__raw = ''
-            function(text)
-              return text:sub(1, 1):upper()
-            end
-          '';
-        }
-      ];
-      lualine_b = [
-        {
-          name = "branch";
-        }
-        {
-          name = "diff";
-          extraConfig = {
+      sections = {
+        lualine_a = [
+          {
+            __unkeyed-1 = "mode";
+            icon = "";
+            fmt.__raw = ''
+              function(text)
+                return text:sub(1, 1):upper()
+              end
+            '';
+          }
+        ];
+        lualine_b = [
+          "branch"
+          {
+            __unkeyed-1 = "diff";
             symbols = {
               added.__raw = "utils.icons.git.added";
               modified.__raw = "utils.icons.git.modified";
@@ -43,130 +42,115 @@ _: {
                 end
               end
             '';
-          };
-        }
-      ];
-      lualine_c = [
-        {
-          name.__raw = "utils.lualine.root_dir().name";
-          extraConfig = {
+          }
+        ];
+        lualine_c = [
+          {
+            __unkeyed-1.__raw = "utils.lualine.root_dir().name";
             cond.__raw = "utils.lualine.root_dir().cond";
             color.__raw = "utils.lualine.root_dir().color";
-          };
-        }
-        {
-          name = "diagnostics";
-          extraConfig.symbols = {
-            error.__raw = "utils.icons.diagnostics.error";
-            warn.__raw = "utils.icons.diagnostics.warn";
-            info.__raw = "utils.icons.diagnostics.info";
-            hint.__raw = "utils.icons.diagnostics.hint";
-          };
-        }
-        {
-          name = "filetype";
-          extraConfig = {
+          }
+          {
+            __unkeyed-1 = "diagnostics";
+            symbols = {
+              error.__raw = "utils.icons.diagnostics.error";
+              warn.__raw = "utils.icons.diagnostics.warn";
+              info.__raw = "utils.icons.diagnostics.info";
+              hint.__raw = "utils.icons.diagnostics.hint";
+            };
+          }
+          {
+            __unkeyed-1 = "filetype";
             icon_only = true;
-          };
-          separator = {
-            left = "";
-            right = "";
-          };
-          padding = {
-            left = 1;
-            right = 0;
-          };
-        }
-        {
-          name.__raw = "utils.lualine.pretty_path()";
-        }
-        {
-          name.__raw = ''
-            utils.lualine.trouble.name
-          '';
-          extraConfig.cond.__raw = ''
-            utils.lualine.trouble.cond
-          '';
-        }
-      ];
+            separator = "";
+            padding = {
+              left = 1;
+              right = 0;
+            };
+          }
+          {
+            __unkeyed-1.__raw = "utils.lualine.pretty_path()";
+          }
+          {
+            __unkeyed-1.__raw = ''
+              utils.lualine.trouble.name
+            '';
+            cond.__raw = ''
+              utils.lualine.trouble.cond
+            '';
+          }
+        ];
 
-      lualine_x = [
-        {
-          name.__raw = ''
-            function()
-              return require("noice").api.status.command.get()
-            end
-          '';
-          extraConfig = {
+        lualine_x = [
+          {
+            __unkeyed-1.__raw = ''
+              function()
+                return require("noice").api.status.command.get()
+              end
+            '';
             cond.__raw = ''
               function()
                 return require("noice").api.status.command.has()
               end
             '';
             color = "Statement";
-          };
-        }
-        {
-          name.__raw = ''
-            function()
-              return require("noice").api.status.mode.get()
-            end
-          '';
-          extraConfig = {
+          }
+          {
+            __unkeyed-1.__raw = ''
+              function()
+                return require("noice").api.status.mode.get()
+              end
+            '';
             cond.__raw = ''
               function()
                 return require("noice").api.status.mode.has()
               end
             '';
             color = "Constant";
-          };
-        }
-        {
-          name.__raw = ''
-            function()
-              return  "  " .. require("dap").status()
-            end
-          '';
-          extraConfig = {
+          }
+          {
+            __unkeyed-1.__raw = ''
+              function()
+                return  "  " .. require("dap").status()
+              end
+            '';
             cond.__raw = ''
               function()
                 return require("dap").status() ~= ""
               end
             '';
             color = "Debug";
-          };
-        }
-      ];
-      lualine_y = [
-        {
-          name = "fileformat";
-          separator.left = "";
-        }
-        {
-          name = "encoding";
-          separator.left = "";
-        }
-        {
-          name = "filesize";
-        }
-      ];
-      lualine_z = [
-        {
-          name = "progress";
-          padding = {
-            left = 1;
-            right = 1;
-          };
-        }
-        {
-          name = "location";
-          separator.left = " ";
-          padding = {
-            left = 0;
-            right = 0;
-          };
-        }
-      ];
+          }
+        ];
+        lualine_y = [
+          {
+            __unkeyed-1 = "fileformat";
+            separator.left = "";
+          }
+          {
+            __unkeyed-1 = "encoding";
+            separator.left = "";
+          }
+          "filesize"
+        ];
+        lualine_z = [
+          {
+            __unkeyed-1 = "progress";
+            padding = {
+              left = 1;
+              right = 1;
+            };
+          }
+          {
+            __unkeyed-1 = "location";
+            separator.left = " ";
+            padding = {
+              left = 0;
+              right = 0;
+            };
+          }
+        ];
+      };
     };
   };
 }
--- home-manager/cli/neovim/extra/coding/copilot.nix
+++ home-manager/cli/neovim/extra/coding/copilot.nix
@@ -25,34 +25,32 @@ in {
         }
       ];
 
-      lualine.sections.lualine_x = [
+      lualine.settings.sections.lualine_x = [
         {
-          name.__raw = ''
+          __unkeyed-1.__raw = ''
             function()
               local status = require("copilot.api").status.data
               local icon = utils.icons.kinds.Copilot
               return icon .. (status.message or "")
             end
           '';
-          extraConfig = {
-            cond.__raw = ''
-              function()
-                local clients = vim.lsp.get_clients({ name = "copilot", bufnr = 0 })
-                return #clients > 0
+          cond.__raw = ''
+            function()
+              local clients = vim.lsp.get_clients({ name = "copilot", bufnr = 0 })
+              return #clients > 0
+            end
+          '';
+          color.__raw = ''
+            function()
+              local status = require("copilot.api").status.data.status
+              if status == "Warning" then
+                return "DiagnosticError"
+              elseif status == "InProgress" then
+                return "DiagnosticWarn"
               end
-            '';
-            color.__raw = ''
-              function()
-                local status = require("copilot.api").status.data.status
-                if status == "Warning" then
-                  return "DiagnosticError"
-                elseif status == "InProgress" then
-                  return "DiagnosticWarn"
-                end
-                return "Special"
-              end
-            '';
-          };
+              return "Special"
+            end
+          '';
         }
       ];
     };
