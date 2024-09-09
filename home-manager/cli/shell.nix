cf88c33464abe34da409500743dd138428979116
Fr4nk1in <fushen@mail.ustc.edu.cn>
2024-09-09 14:01:44 +0800

    [shell] use native zsh plugin management

--- home-manager/cli/shell.nix
+++ home-manager/cli/shell.nix
@@ -1,4 +1,4 @@
-_: {
+{pkgs, ...}: {
   programs.zsh = {
     enable = true;
     enableCompletion = true;
@@ -33,14 +33,25 @@ _: {
       ":q" = "exit";
     };
 
-    antidote = {
-      enable = true;
-      plugins = [
-        "wbingli/zsh-wakatime"
-        "Aloxaf/fzf-tab"
-        "chisui/zsh-nix-shell"
-      ];
-    };
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        src = "${pkgs.zsh-nix-shell}/share/zsh-nix-shell";
+      }
+      {
+        name = "fzf-tab";
+        src = "${pkgs.zsh-fzf-tab}/share/fzf-tab";
+      }
+      {
+        name = "zsh-wakatime";
+        src = pkgs.fetchFromGitHub {
+          owner = "wbingli";
+          repo = "zsh-wakatime";
+          rev = "e0d1dfcaaab11112344e14b826f3136edac4eccc";
+          hash = "sha256-QN/MUDm+hVJUMA4PDqs0zn9XC2wQZrgQr4zmCF0Vruk=";
+        };
+      }
+    ];
 
     initExtra = "setopt interactivecomments";
   };
