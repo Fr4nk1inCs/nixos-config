b33204c481d4bbf6b0354d2fe709973d556ec965
Fr4nk1in <fushen@mail.ustc.edu.cn>
2024-10-08 22:50:51 +0800

    [darwin] move home-manager packages to system packages, use maintained configs as possible

--- home-manager/config/gui/softwares.nix
+++ home-manager/config/gui/softwares.nix
@@ -5,66 +5,42 @@
   ...
 }: let
   cfg = config.homeManagerConfig;
-  enable = cfg.gui.enable && cfg.gui.software.enable;
-  programsForAllSystems = with pkgs; [
-    # browser
-    google-chrome
-    # instant messaging
-    telegram-desktop
-    # streaming music
-    spotify
-    # remote code editing
-    vscode
-  ];
-  programsForLinux = with pkgs; [
-    # screen capture
-    obs-studio
-    # file syncing
-    onedrive
-    # document viewer
-    evince
-    zathura
-    # note taking
-    obsidian
-    # video player
-    mpv
-    vlc
-    # remote desktop
-    parsec-bin
-    # image editing
-    inkscape
-    # academic research
-    zotero
-    # instant messaging
-    qq
-    # file explorer
-    nautilus
-    # system management
-    baobab # disk usage analyzer
-  ];
-  programsForDarwin = with pkgs; [
-    # browser
-    arc-browser
-    # app launcher
-    raycast
-    # menubar utility
-    ice-bar
-    stats
-    # battery management
-    aldente
-    # pdf reader
-    skimpdf
-    # input method
-    sogou-pinyin-mac
-    # Knowledge base
-    notion-for-apple-silicon
-    # online meeting
-    tencent-meeting
-  ];
-  programs =
-    programsForAllSystems
-    ++ lib.optionals (cfg.system != "darwin") programsForLinux
-    ++ lib.optionals (cfg.system == "darwin") programsForDarwin;
+  enable = cfg.gui.enable && cfg.gui.software.enable && cfg.system == "linux";
 in {
-  config.home.packages = lib.optionals enable programs;
+  config.home.packages = lib.optionals enable (
+    with pkgs; [
+      # browser
+      google-chrome
+      # instant messaging
+      telegram-desktop
+      # streaming music
+      spotify
+      # remote code editing
+      vscode
+      # screen capture
+      obs-studio
+      # file syncing
+      onedrive
+      # document viewer
+      evince
+      zathura
+      # note taking
+      obsidian
+      # video player
+      mpv
+      vlc
+      # remote desktop
+      parsec-bin
+      # image editing
+      inkscape
+      # academic research
+      zotero
+      # instant messaging
+      qq
+      # file explorer
+      nautilus
+      # system management
+      baobab # disk usage analyzer
+    ]
+  );
 }
