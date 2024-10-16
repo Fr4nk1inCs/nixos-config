e2e06c6ceb9bf43a332b90410edd9f5879c14bfb
Fr4nk1in <fushen@mail.ustc.edu.cn>
2024-10-17 01:32:37 +0800

    [nixvim] make rust-analyzer ignore some dirs

--- home-manager/config/cli/neovim/core/plugins/langs/rust.nix
+++ home-manager/config/cli/neovim/core/plugins/langs/rust.nix
@@ -12,32 +12,63 @@
     rustaceanvim = {
       enable = true;
       settings = {
-        server.on_attach.__raw = ''
-          function(client, bufnr)
-            vim.keymap.set("n", "<leader>cR", function()
-              vim.cmd.RustLsp("codeAction")
-            end, { desc = "Code Action", buffer = bufnr })
-            vim.keymap.set("n", "<leader>dr", function()
-              vim.cmd.RustLsp("debuggables")
-            end, { desc = "Rust Debuggables", buffer = bufnr })
-            return _M.lspOnAttach(client, bufnr)
-          end
-        '';
-        default_settings.rust-analyzer = {
-          cargo = {
-            allFeatures = true;
-            loadOutDirsFromCheck = true;
-            buildScripts = {
-              enable = true;
-            };
+        server = {
+          capabilities = {
+            # https://github.com/hrsh7th/cmp-nvim-lsp/issues/44#issuecomment-2096368152
+            workspace.didChangeWatchedFiles.dynamicRegistration = true;
           };
-          checkOnSave = true;
-          procMacro = {
-            enable = true;
-            ignored = {
-              async-trait = ["async_trait"];
-              napi-derive = ["napi"];
-              async-recursion = ["async_recursion"];
+          on_attach.__raw = ''
+            function(client, bufnr)
+              vim.keymap.set("n", "<leader>cR", function()
+                vim.cmd.RustLsp("codeAction")
+              end, { desc = "Code Action", buffer = bufnr })
+              vim.keymap.set("n", "<leader>dr", function()
+                vim.cmd.RustLsp("debuggables")
+              end, { desc = "Rust Debuggables", buffer = bufnr })
+              return _M.lspOnAttach(client, bufnr)
+            end
+          '';
+          default_settings.rust-analyzer = {
+            cargo = {
+              allFeatures = true;
+              loadOutDirsFromCheck = true;
+              buildScripts = {
+                enable = true;
+              };
+            };
+            checkOnSave = true;
+            files = {
+              excludeDirs = [
+                "_build"
+                ".dart_tool"
+                ".direnv"
+                ".flatpak-builder"
+                ".git"
+                ".gitlab"
+                ".gitlab-ci"
+                ".gradle"
+                ".idea"
+                ".next"
+                ".project"
+                ".scannerwork"
+                ".settings"
+                ".venv"
+                "archetype-resources"
+                "bin"
+                "hooks"
+                "node_modules"
+                "po"
+                "screenshots"
+                "target"
+              ];
+            };
+            procMacro = {
+              enable = true;
+              ignored = {
+                async-trait = ["async_trait"];
+                napi-derive = ["napi"];
+                async-recursion = ["async_recursion"];
+              };
             };
           };
         };
