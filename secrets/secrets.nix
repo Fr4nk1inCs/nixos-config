0170d83d704e72b0ef1f1092d90ced47a99ccee8
Fr4nk1in <fushen@mail.ustc.edu.cn>
2025-02-12 00:37:38 +0800

    [secret] add wsl key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,14 +1,16 @@
 let
   users = {
-    fr4nk1in = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJS2YtTqHmRishJaLDp1cKKrzM9PjxG/ZLKCCU7g5pkz fushen@mail.ustc.edu.cn";
+    darwin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJS2YtTqHmRishJaLDp1cKKrzM9PjxG/ZLKCCU7g5pkz";
+    wsl = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMP+Fa4Yhx/APEKYEGxpUb3Z6iPLbwhaTCTPK2w49aq2";
   };
   allUsers = builtins.attrValues users;
 
   systems = {
-    darwin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBNPhGQ0q6Sxuitk0KiqgDtEebhdsy2l92a2OiAiJiWd Fr4nk1in's MacBook Air";
+    darwin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBNPhGQ0q6Sxuitk0KiqgDtEebhdsy2l92a2OiAiJiWd";
+    wsl = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDFChdda78yatiqDYUFcPwavnE47IwtrK55taQ2VPJCm";
   };
   allSystems = builtins.attrValues systems;
 in {
   "deepseek-apikey.age".publicKeys = allUsers ++ allSystems;
-  "fr4nk1in-ed25519.age".publicKeys = [users.fr4nk1in] ++ allSystems;
+  "fr4nk1in-ed25519.age".publicKeys = allUsers ++ allSystems;
 }
