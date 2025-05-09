457bfe28910c5a48e979a634e65ed5c99e4d1dcf
Fr4nk1in <fushen@mail.ustc.edu.cn>
2025-05-09 10:53:28 +0800

    [nix] remove SJTU cache

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -20,8 +20,6 @@
     substituters = [
       # status: https://mirrors.ustc.edu.cn/status
       "https://mirrors.ustc.edu.cn/nix-channels/store"
-      # status: https://mirror.sjtu.edu.cn/
-      "https://mirror.sjtu.edu.cn/nix-channels/store"
 
       "https://cache.nixos.org"
 
