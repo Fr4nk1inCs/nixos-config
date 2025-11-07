f7949fe552854db03663ede9762ffe0ecf138624
Fr4nk1in <fushen@mail.ustc.edu.cn>
2025-11-08 00:02:24 +0800

    [git] fix lazygit and add delta configuration

--- home-manager/config/cli/git.nix
+++ home-manager/config/cli/git.nix
@@ -20,6 +20,18 @@ _: {
         navigate = true;
         light = false;
         side-by-side = false;
+        # line numbers
+        line-numbers = true;
+        line-numbers-minus-style = "red";
+        line-numbers-plus-style = "green";
+        # theme
+        syntax-theme = "Nord";
+        minus-style = "syntax #56404b";
+        minus-emph-style = "syntax #56404b";
+        minus-non-emph-style = "syntax #3f4247";
+        plus-style = "syntax #4e5a55";
+        plus-emph-style = "syntax #4e5a55";
+        plus-non-emph-style = "syntax #3f4247";
       };
     };
 
@@ -44,7 +56,7 @@ _: {
           };
         };
 
-        git.paging.pager = "delta --dark --paging=never";
+        git.pagers = [{pager = "delta --dark --paging=never";}];
 
         parseEmoji = true;
       };
