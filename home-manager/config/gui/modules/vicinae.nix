cc2c6c11040223b6446c14f0c5d4908710efda7f
Fr4nk1in <fushen@mail.ustc.edu.cn>
2025-11-13 03:46:38 +0800

    [gui] persist vicinae config

--- home-manager/config/gui/modules/vicinae.nix
+++ home-manager/config/gui/modules/vicinae.nix
@@ -8,5 +8,26 @@ in {
   services.vicinae = {
     inherit enable;
     autoStart = false;
+
+    settings = {
+      closeOnFocusLoss = true;
+      faviconService = "twenty";
+      font = {
+        normal = config.stylix.fonts.monospace.name;
+        size = config.stylix.fonts.sizes.applications;
+      };
+      theme = {
+        iconTheme = config.stylix.icons.dark;
+        name = "nord";
+      };
+      keybinding = "default";
+      popToRootOnClose = true;
+      rootSearch.searchFiles = true;
+      window = {
+        csd = true;
+        opacity = config.stylix.opacity.applications;
+        rounding = 10;
+      };
+    };
   };
 }
