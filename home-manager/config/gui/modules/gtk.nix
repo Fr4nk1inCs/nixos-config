3be6a9616315d9e7e761a1ee9855dba966d3f49f
Fr4nk1in <fushen@mail.ustc.edu.cn>
2025-11-12 22:12:34 +0800

    [gtk] patch libadwaita

--- home-manager/config/gui/modules/gtk.nix
+++ home-manager/config/gui/modules/gtk.nix
@@ -5,14 +5,39 @@
   ...
 }: let
   enable = config.homeManagerConfig.gui.enable && pkgs.stdenv.isLinux;
+  whitesur-gtk-theme =
+    (pkgs.whitesur-gtk-theme.override {
+      schemeVariants = ["nord"];
+    }).overrideAttrs (_: prev: {
+      pname = "whitesur-gtk-theme-libadwaita-patch";
+      postPatch =
+        prev.postPatch
+        + ''
+          substituteInPlace libs/lib-install.sh --replace-fail '"''${HOME}/.config/gtk-4.0"' '"$out/config/gtk-4.0"'
+          substituteInPlace install.sh --replace-fail '"''${libadwaita}"' '"true"'
+          substituteInPlace install.sh --replace-fail '"$UID"' '"1"'
+        '';
+    });
 in {
   gtk = {
     inherit enable;
     theme = lib.mkForce {
       name = "WhiteSur-Dark-nord";
-      package = pkgs.whitesur-gtk-theme.override {
-        schemeVariants = ["nord"];
-      };
+      package = whitesur-gtk-theme;
+    };
+  };
+
+  xdg.configFile = lib.mkIf enable {
+    "gtk-4.0/gtk.css" = lib.mkForce {
+      source = "${whitesur-gtk-theme}/config/gtk-4.0/gtk.css";
+    };
+    "gtk-4.0/assets" = lib.mkForce {
+      source = "${whitesur-gtk-theme}/config/gtk-4.0/assets";
+      recursive = true;
+    };
+    "gtk-4.0/windows-assets" = lib.mkForce {
+      source = "${whitesur-gtk-theme}/config/gtk-4.0/windows-assets";
+      recursive = true;
     };
   };
 }
