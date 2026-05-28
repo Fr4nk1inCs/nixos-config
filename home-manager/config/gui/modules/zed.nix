45aab9789644a00b414d633242429ccc1256ce7f
Fr4nk1in <fushen@mail.ustc.edu.cn>
2026-05-28 21:38:49 +0800

    [zed] update config

--- home-manager/config/gui/modules/zed.nix
+++ home-manager/config/gui/modules/zed.nix
@@ -1,5 +1,4 @@
 {
-  pkgs,
   config,
   lib,
   ...
@@ -7,138 +6,199 @@
   inherit (config.profile.guiSoftwares) enable;
 in {
   config = {
-    home.packages = lib.optionals enable (with pkgs; [zed-editor nixd nixfmt]);
+    programs.zed-editor = {
+      inherit enable;
 
-    xdg.configFile."zed/keymap.json".text = builtins.toJSON [
-      {
-        context = "Workspace";
-        bindings = {"ctrl-\\" = "terminal_panel::ToggleFocus";};
-      }
-      {
-        "context" = "vim_mode == normal && !menu";
-        "bindings" = {
-          # navigation
+      installRemoteServer = true;
+
+      mutableUserDebug = true;
+      mutableUserSettings = true;
+
+      userKeymaps = let
+        navigation = {
           "H" = "pane::ActivatePreviousItem";
           "L" = "pane::ActivateNextItem";
           "ctrl-h" = "workspace::ActivatePaneLeft";
           "ctrl-l" = "workspace::ActivatePaneRight";
           "ctrl-k" = "workspace::ActivatePaneUp";
           "ctrl-j" = "workspace::ActivatePaneDown";
-          # yank
-          "Y" = ["workspace::SendKeystrokes" "y $"];
-          # move lines
-          "alt-j" = "editor::MoveLineDown";
-          "alt-k" = "editor::MoveLineUp";
-          # buffer
-          "b d" = "pane::CloseActiveItem";
-          "b o" = "pane::CloseInactiveItems";
-          # file
-          "space e" = "project_panel::ToggleFocus";
-          "space f e" = "project_panel::ToggleFocus";
-          "space s e" = "outline_panel::ToggleFocus";
-          "space f f" = "file_finder::Toggle";
-          # search
-          "space s g" = "workspace::NewSearch";
-          "space s s" = "outline::Toggle";
-          # git
-          "space g b" = "git::Blame";
-          # code
-          "space c d" = "editor::Hover";
-          "space c f" = "editor::Format";
-          "space c a" = "editor::ToggleCodeActions";
-          "space c r" = "editor::Rename";
-          "g d" = "editor::GoToDefinition";
-          "g p d" = "editor::GoToDefinitionSplit";
-          "g r" = "editor::FindAllReferences";
-          "g y" = "editor::GoToTypeDefinition";
-          "g p y" = "editor::GoToTypeDefinitionSplit";
-          "g I" = "editor::GoToImplementation";
-          "g p I" = "editor::GoToImplementationSplit";
         };
-      }
-      {
-        "context" = "vim_mode == visual && !menu";
-        "bindings" = {">" = "editor::Indent";};
-      }
-      {
-        "context" = "Editor";
-        "bindings" = {
-          "ctrl-a" = "editor::AcceptEditPrediction";
-          "ctrl-[" = "copilot::PreviousSuggestion";
-          "ctrl-]" = "copilot::NextSuggestion";
-        };
-      }
-      {
-        "context" = "ProjectPanel && not_editing";
-        "bindings" = {
-          "space e" = "workspace::ToggleLeftDock";
-          "space s e" = "outline_panel::ToggleFocus";
-          "q" = "workspace::ToggleLeftDock";
-          "a" = "project_panel::NewFile";
-          "A" = "project_panel::NewDirectory";
-          "x" = "project_panel::Cut";
-          "y" = "project_panel::Copy";
-          "p" = "project_panel::Paste";
-          "d" = "project_panel::Delete";
-          "r" = "project_panel::Rename";
-        };
-      }
-      {
-        "context" = "OutlinePanel && not_editing";
-        "bindings" = {
-          "space e" = "project_panel::ToggleFocus";
-          "space f e" = "project_panel::ToggleFocus";
-          "space s e" = "workspace::ToggleLeftDock";
-          "q" = "workspace::ToggleLeftDock";
-        };
-      }
-      {
-        "context" = "Editor && (showing_code_actions || showing_completions)";
-        "bindings" = {
-          "enter" = "editor::ConfirmCompletionInsert";
-          "tab" = "editor::ContextMenuNext";
-          "shift-tab" = "editor::ContextMenuPrevious";
-          "up" = "editor::ContextMenuPrevious";
-          "down" = "editor::ContextMenuNext";
-          "ctrl-p" = "editor::ContextMenuPrevious";
-          "ctrl-n" = "editor::ContextMenuNext";
-        };
-      }
-    ];
 
-    # user settings should be modifiable since zed constantly updates it
-    # xdg.configFile."zed/settings.json".text = {
-    #   vim_mode = true;
-    #   relative_line_numbers = true;
-    #   command_aliases = {
-    #     W = "w";
-    #     Wq = "wq";
-    #     Q = "q";
-    #   };
-    #   # Appearance
-    #   ui_font_size = 14;
-    #   buffer_font_family = "Maple Mono NF CN";
-    #   buffer_font_size = 11;
-    #   buffer_font_features = {
-    #     calt = true;
-    #     zero = false;
-    #     cv01 = true;
-    #     cv02 = false;
-    #     cv03 = false;
-    #     cv04 = false;
-    #     cv98 = false;
-    #     cv99 = false;
-    #     ss01 = false;
-    #   };
-    #   theme = {
-    #     mode = "system";
-    #     light = "Dawnfox";
-    #     dark = "Nordfox";
-    #   };
-    #   tab_size = 2;
-    #   wrap_guides = [80 120];
-    #   # code
-    #   preferred_line_length = 80;
-    # };
+        finders = {
+          "space s g" = "workspace::NewSearch";
+          "space f e" = "project_panel::ToggleFocus";
+          "space f f" = "file_finder::Toggle";
+        };
+
+        code = {
+          "space g b" = "git::Blame";
+
+          "space c a" = "editor::ToggleCodeActions";
+          "space c f" = "editor::Format";
+          "g d" = "editor::GoToDefinition";
+          "g r a" = "editor::ToggleCodeActions";
+          "g r r" = "editor::FindAllReferences";
+          "g r n" = "editor::Rename";
+          "g r t" = "editor::GoToTypeDefinition";
+          "g r i" = "editor::GoToImplementation";
+          "g p d" = "editor::GoToDefinitionSplit";
+          "g p t" = "editor::GoToTypeDefinitionSplit";
+          "g p i" = "editor::GoToImplementationSplit";
+          "g O" = "outline::Toggle";
+        };
+
+        QOL = {
+          "Y" = ["workspace::SendKeystrokes" "y $"];
+          "alt-d" = "editor::MoveLineDown";
+          "alt-u" = "editor::MoveLineUp";
+        };
+      in [
+        {
+          "context" = "VimControl && !menu";
+          "bindings" = {
+            "space" = null;
+          };
+        }
+
+        {
+          context = "Workspace";
+          bindings = {"ctrl-\\" = "terminal_panel::ToggleFocus";};
+        }
+
+        {
+          "context" = "vim_mode == normal && !menu";
+          "bindings" =
+            {
+              "b d" = "pane::CloseActiveItem";
+              "b o" = "pane::CloseOtherItems";
+
+              "space e" = "project_panel::ToggleFocus";
+              "space f e" = "project_panel::ToggleFocus";
+              "space f o" = "outline_panel::ToggleFocus";
+            }
+            // code // navigation // finders // QOL;
+        }
+
+        {
+          "context" = "vim_mode == visual && !menu";
+          "bindings" = {">" = "editor::Indent";};
+        }
+
+        {
+          "context" = "Editor";
+          "bindings" = {
+            "ctrl-;" = "editor::AcceptEditPrediction";
+            "ctrl-[" = "copilot::PreviousSuggestion";
+            "ctrl-]" = "copilot::NextSuggestion";
+          };
+        }
+
+        {
+          "context" = "!Editor && !Terminal";
+          "bindings" =
+            {
+              "q" = "workspace::CloseActiveDock";
+            }
+            // finders;
+        }
+
+        {
+          "context" = "!Editor && !Terminal";
+          "unbind" = {
+            "space f" = "file_finder::Toggle";
+          };
+        }
+
+        {
+          "context" = "ProjectPanel && not_editing";
+          "bindings" = {
+            "space e" = "workspace::CloseActiveDock";
+            "space f e" = "workspace::CloseActiveDock";
+            "space f o" = "outline_panel::ToggleFocus";
+            "a" = "project_panel::NewFile";
+            "A" = "project_panel::NewDirectory";
+            "x" = "project_panel::Cut";
+            "y" = "project_panel::Copy";
+            "p" = "project_panel::Paste";
+            "d" = "project_panel::Delete";
+            "r" = "project_panel::Rename";
+          };
+        }
+
+        {
+          "context" = "OutlinePanel && not_editing";
+          "bindings" = {
+            "space f o" = "workspace::CloseActiveDock";
+            "space f e" = "project_panel::ToggleFocus";
+          };
+        }
+
+        {
+          "context" = "Editor && (showing_code_actions || showing_completions)";
+          "bindings" = {
+            "enter" = "editor::ConfirmCompletionInsert";
+            "tab" = "editor::ContextMenuNext";
+            "shift-tab" = "editor::ContextMenuPrevious";
+            "up" = "editor::ContextMenuPrevious";
+            "down" = "editor::ContextMenuNext";
+            "ctrl-p" = "editor::ContextMenuPrevious";
+            "ctrl-n" = "editor::ContextMenuNext";
+          };
+        }
+      ];
+
+      userSettings = let
+        font-features = {
+          calt = true;
+          zero = false;
+          cv01 = true;
+          cv02 = false;
+          cv03 = false;
+          cv04 = false;
+          cv98 = false;
+          cv99 = false;
+          ss01 = false;
+        };
+      in {
+        vim_mode = true;
+        vim.toggle_relative_line_numbers = true;
+        vim.use_system_clipboard = "always";
+        which_key.enabled = true;
+        which_key.delay_ms = 0;
+
+        command_aliases = {
+          W = "w";
+          Wq = "wq";
+          Q = "q";
+        };
+
+        title_bar.show_branch_status_icon = true;
+
+        ui_font_family = lib.mkForce config.stylix.fonts.monospace.name;
+        ui_font_features = font-features;
+        ui_font_size = lib.mkForce config.stylix.fonts.sizes.terminal;
+        buffer_font_features = font-features;
+        buffer_font_size = lib.mkForce config.stylix.fonts.sizes.terminal;
+
+        wrap_guides = [80 120];
+        preferred_line_length = 80;
+
+        edit_predictions = {
+          provider = "copilot";
+          mode = "eager";
+          disabled_globs = [];
+          copilot = {
+            proxy = null;
+            proxy_no_verify = null;
+            enterprise_uri = null;
+          };
+        };
+
+        agent_servers = {
+          claude-acp.type = "registry";
+          pi-acp.type = "registry";
+        };
+      };
+    };
   };
 }
